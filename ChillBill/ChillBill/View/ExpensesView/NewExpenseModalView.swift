2eacf1d429aa51b0d55a030ad21a9e06ef3438da
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2024-08-01 14:56:18 -0400

    added ScrollView to the NewExpenseModalView

--- ChillBill/ChillBill/View/ExpensesView/NewExpenseModalView.swift
+++ ChillBill/ChillBill/View/ExpensesView/NewExpenseModalView.swift
@@ -3,37 +3,52 @@ import SwiftUI
 struct NewExpenseModalView: View {
     //MARK: - Internal interface
     
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        VStack(spacing: spaceBetweenTextFields) {
-            LabelWithTextFieldView(
-                input: $title,
-                labelText: "Title",
-                placeholderText: "What was it?",
-                keyboardType: .default
-            )
-            
-            LabelWithTextFieldView(
-                input: $amount,
-                labelText: "Amount",
-                placeholderText: "0.0",
-                keyboardType: .decimalPad
-            )
-            
-            LabelWithTextFieldView(
-                input: $paidBy,
-                labelText: "Paid by:",
-                placeholderText: "Who paid the bill?",
-                keyboardType: .default
-            )
-            
-            LabelWithDatePickerView(date: $date, labelText: "Date")
-            
-            Spacer()
-            
-            Button("Add", action: {})
-                .buttonStyle(OrangeRoundedRectangleButton())
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: spaceBetweenTextFields) {
+                    LabelWithTextFieldView(
+                        input: $title,
+                        labelText: "Title",
+                        placeholderText: "What was it?",
+                        keyboardType: .default
+                    )
+                    
+                    LabelWithTextFieldView(
+                        input: $amount,
+                        labelText: "Amount",
+                        placeholderText: "0.0",
+                        keyboardType: .decimalPad
+                    )
+                    
+                    LabelWithTextFieldView(
+                        input: $paidBy,
+                        labelText: "Paid by:",
+                        placeholderText: "Who paid the bill?",
+                        keyboardType: .default
+                    )
+                    
+                    LabelWithDatePickerView(date: $date, labelText: "Date")
+                    
+                    Spacer()
+                    
+                    Button("Add", action: {})
+                        .buttonStyle(OrangeRoundedRectangleButton())
+                }
+                .padding()
+                .navigationTitle(titleOfTheModal)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Cancel") {
+                            dismiss()
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
     
     //MARK: - Private inteface
@@ -43,6 +58,7 @@ struct NewExpenseModalView: View {
     @State private var paidBy = ""
     @State private var date = Date()
     private let spaceBetweenTextFields: CGFloat = 20
+    private let titleOfTheModal = "New expense"
 }
 
 #Preview {
