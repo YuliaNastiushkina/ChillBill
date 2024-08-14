c1405df2faf93d9ad295f65ec4acbe9bbbf804a3
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2024-08-14 12:29:58 -0400

    updated RealmManager,  added function `addObject`

--- ChillBill/ChillBill/Model/Realm/RealmManager.swift
+++ ChillBill/ChillBill/Model/Realm/RealmManager.swift
@@ -7,10 +7,7 @@ final class RealmManager {
     // MARK: - Internal interface
     
     /// The shared singleton instance of `RealmManager
-    static let shared: RealmManager = {
-        let manager = RealmManager()
-        return manager
-    }()
+    static let shared = RealmManager()
     
     // MARK: - Private interface
     private(set) var realm: Realm?
@@ -26,10 +23,34 @@ final class RealmManager {
         }
     }
     
+    /// Writes a block of code to the Realm database.
+    /// - Parameter block: A closure containing the operations to be performed within the write transaction.
+    private func write(_ block: () -> Void) {
+        guard let realm = realm else {
+            print("Realm is not initialized, cannot write to database.")
+            return
+        }
+        do {
+            try realm.write {
+                block()
+            }
+        } catch {
+            print("Failed to write to Realm: \(error)")
+        }
+    }
+    
     // MARK: - Internal methods
     /// Checks whether `Realm` has been successfully initialized.
     /// - Returns: `true` if `Realm` has been successfully initialized or `false` if not.
     func isRealmInitialized() -> Bool {
         return realm != nil
+    }
+    
+    /// Adds an object to the Realm database.
+    /// - Parameter object: The object to be added to the database.
+    func addObject(_ object: Object) {
+        write {
+            realm?.add(object)
+        }
     }
 }
